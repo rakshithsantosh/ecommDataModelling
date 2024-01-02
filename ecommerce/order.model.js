311b4dea09ad38bef8bb4bdf9f3767a76a7113da
rakshith_s <rakshithsantosh@gmail.com>
2024-01-02 11:28:01 +0530

    ecomm data model

---
+++ ecommerce/order.model.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+
+const orderItemSchema = mongoose.schema(
+    {
+        productId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Product'
+        },
+        quantity:{
+            type:Number,
+            required:true
+        }
+    }
+)
+
+const orderSchema = mongoose.Schema(
+    {
+        orderPrice:{
+            type:Number,
+            required:true
+        },
+        customer:{
+            type:mongoose.Schema.types.ObjectId,
+            ref:"User"
+        },
+        orderItems:{
+            type:[orderItemSchema]
+        },
+        address:{
+            type:String,
+            required:true
+        },
+        status:{
+            type:String,
+            enum: ["PENDING","CANCELLED","DELIVERED"],
+            default:"PENDING"
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+export const Order = mongoose.model("Order",orderSchema)
